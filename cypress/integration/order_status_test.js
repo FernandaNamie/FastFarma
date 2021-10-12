55e62bdaca0b49b9af6bec4a2128fa3ef1399161
Leticia Cohen Ferrari <leticia.cohen@inspira.com.br>
2021-10-12 18:37:17 -0300

    Parametriza valores esperados dos testes automatizados (OrderStatus)

--- cypress/integration/order_status_test.js
+++ cypress/integration/order_status_test.js
@@ -5,31 +5,47 @@ describe('Navegação e Funcionalidade', () => {
     })
     /** */
     it('lista de produtos deve ter 2 elementos iniciais', () => {
-        cy.get('[id="products"]').children().should('have.length', 2)
+        cy.fixture('pedido').then((pedido) => {
+            cy.get('[id="products"]').children().should('have.length', pedido.products.length)
+        })
     })
     /** */
     it('preço do primeiro produto', () => {
-        cy.get('[id="product-0"]').invoke('attr', 'price').should('eq', '20')
+        cy.fixture('pedido').then((pedido) => {
+            cy.get('[id="product-0"]').invoke('attr', 'price').should('eq', pedido.products[0].price.toString())
+        })
     })
     /** */
     it('quantidade do primeiro produto', () => {
-        cy.get('[id="product-0"]').invoke('attr', 'amount').should('eq', '1')
+        cy.fixture('pedido').then((pedido) => {
+            cy.get('[id="product-0"]').invoke('attr', 'amount').should('eq', pedido.products[0].amount.toString())
+        })
     })
     /** */
     it('preço do segundo produto', () => {
-        cy.get('[id="product-1"]').invoke('attr', 'price').should('eq', '5')
+        cy.fixture('pedido').then((pedido) => {
+            cy.get('[id="product-1"]').invoke('attr', 'price').should('eq', pedido.products[1].price.toString())
+        })
     })
     /** */
     it('quantidade do segundo produto', () => {
-        cy.get('[id="product-1"]').invoke('attr', 'amount').should('eq', '2')
+        cy.fixture('pedido').then((pedido) => {
+            cy.get('[id="product-1"]').invoke('attr', 'amount').should('eq', pedido.products[1].amount.toString())
+        })
     })
     /** */
     it('preço do frete', () => {
-        cy.get('[id="frete"]').invoke('attr', 'frete').should('eq', '6')
+        cy.get('[id="frete"]').invoke('attr', 'frete').should('eq', '6')  // valor fixado
     })
     /** */
     it('preço total da compra', () => {
-        cy.get('[id="total"]').invoke('attr', 'total').should('eq', '36')
+        cy.fixture('pedido').then((pedido) => {
+            let product0 = pedido.products[0].price * pedido.products[0].amount
+            let product1 = pedido.products[1].price * pedido.products[1].amount
+            const frete = 6 // valor fixado
+            let total = product0 + product1 + frete
+            cy.get('[id="total"]').invoke('attr', 'total').should('eq', total.toString())
+        })
     })
     /** */
 })
