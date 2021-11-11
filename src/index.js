2e60610f620f125d38c0ca0a7e7a35d7c0e0507a
Leticia Cohen Ferrari <leticia.cohen@inspira.com.br>
2021-11-11 08:31:24 -0300

    Intercepta todos os requests e reponses e coloca token no header dos requests sempre que ele existir

--- src/index.js
+++ src/index.js
@@ -11,6 +11,20 @@ import ProductRegister from './pages/drugstore/ProductRegister';
 import DrugStoreRegister from './pages/drugstore/DrugStoreRegister';
 import MotoboyRegister from './pages/motoboy/MotoboyRegister';
 import Home from './pages/Home'
+import axios from 'axios'
+
+axios.interceptors.request.use(request => {
+  var token = localStorage.getItem("token")
+  if (token !== "") {
+    request.headers.Authorization = `Bearer ${localStorage.getItem("token")}`
+  }
+  console.log(request)
+  return request
+})
+axios.interceptors.response.use(response => {
+  console.log(response)
+  return response
+})
 ReactDOM.render(
   <BrowserRouter>
     <Switch>
